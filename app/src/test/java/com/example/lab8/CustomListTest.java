309d274b5e0769fa586cab446cd95e8479751b29
Andrew Rosario <71676291+andrew-rosario@users.noreply.github.com>
2023-03-10 12:07:40 -0700

    Add hasCityTest in CustomListTest

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -2,6 +2,7 @@ package com.example.lab8;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 //import org.junit.Before;
@@ -29,6 +30,14 @@ public class CustomListTest {
         int listSize = list.getCount();
         list.addCity(new City("Estevan", "SK"));
         assertEquals(list.getCount(),listSize+1);
+    }
+
+    @Test
+    public void hasCityTest(){
+        list = MockCityList();
+        assertEquals(0,list.getCount());
+        list.addCity(list.addCity(new City("Estevan", "SK"));
+        assertTrue(list.hasCity());
     }
 
 }
